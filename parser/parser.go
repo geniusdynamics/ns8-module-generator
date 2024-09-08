23c4d994c76ba884babb5bb014cd5bde685cc9a2
kemboi22 <kemboielvis22@gmail.com>
2024-09-08 13:34:54 +0300

    feat(parser): added comments

--- parser/parser.go
+++ parser/parser.go
@@ -1,4 +1,4 @@
-package main
+package parser
 
 import (
 	"gopkg.in/yaml.v3"
@@ -12,11 +12,21 @@ type Compose struct {
 	Networks map[string]interface{} `yaml:"networks"`
 }
 
-func parseDockerCompose(filePath string) (map[string]interface{}, map[string]interface{}, map[string]interface{}, error) {
+func ParseDockerCompose(filePath string) (map[string]interface{}, map[string]interface{}, map[string]interface{},
+	error) {
+	/*
+		Open the file and read the content
+	*/
 	composeFile, e := os.Open(filePath)
+	/*
+		Check if there is an error
+	*/
 	if e != nil {
 		return nil, nil, nil, e
 	}
+	/*
+		Defer the closing of the file
+	*/
 	defer func(composeFile *os.File) {
 		err := composeFile.Close()
 		if err != nil {
@@ -26,8 +36,12 @@ func parseDockerCompose(filePath string) (map[string]interface{}, map[string]int
 	byteValue, _ := io.ReadAll(composeFile)
 	var compose Compose
 	e = yaml.Unmarshal(byteValue, &compose)
+	// Check if there is an error
 	if e != nil {
 		return nil, nil, nil, e
 	}
+	/*
+		Return the services, volumes and networks
+	*/
 	return compose.Services, compose.Volumes, compose.Networks, nil
 }
