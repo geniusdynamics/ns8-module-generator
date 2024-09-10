e99c17eb15a6af07cb00280ece7a1f3fe5f72a77
kemboi22 <kemboielvis22@gmail.com>
2024-09-10 23:30:08 +0300

    feat(parser): added Search and write file

--- parser/template_parser.go
+++ parser/template_parser.go
@@ -1,1 +1,64 @@
 package parser
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+/*
+List Files Read the files in the template folder
+*/
+func listFiles(folder string) error {
+	return filepath.WalkDir(folder, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		// Print whether it is a file or a directory
+		if d.IsDir() {
+			fmt.Printf("Directory: %s\n", path)
+		} else {
+			fmt.Printf("File: %s\n", path)
+		}
+
+		return nil
+	})
+}
+
+func SearchFileAndWriteContent(folder string, filename string, content string) error {
+	var filePath string
+	/*
+		Search for the file in the folder*
+	*/
+	err := filepath.WalkDir(folder, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		// Check current entry is a file
+		if !d.IsDir() && d.Name() == filename {
+			filePath = path
+			return filepath.SkipDir
+		}
+		return nil
+	})
+	// Check if an error occurred while searching for the file
+	if err != nil {
+		// if the error is not a file not found error, create the file
+
+		return fmt.Errorf("error while searching for the file: %v", err)
+	}
+	// Check if the file was found
+	if filePath == "" {
+		return fmt.Errorf("file not found: %s", filename)
+	}
+	// Write the content to the file
+	err = os.WriteFile(filePath, []byte(content), 0644)
+	if err != nil {
+		return fmt.Errorf("error while writing to the file: %v", err)
+	}
+	// Return nil if everything is successful
+	fmt.Printf("Content written to file: %s\n", filePath)
+
+	return nil
+}
