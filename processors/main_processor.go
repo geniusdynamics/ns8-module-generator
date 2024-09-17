bc122ecf45081a9005a7492800f8d809600c79e2
kemboi22 <kemboielvis22@gmail.com>
2024-09-18 01:36:49 +0300

    feat(processors): added main processor

---
+++ processors/main_processor.go
@@ -0,0 +1,20 @@
+package processors
+
+import (
+	"fmt"
+	"ns8-module-generator/parser"
+)
+
+func ProcessNs8Module() {
+	// Create a temp Directory
+	err := CopyDirectory()
+	if err != nil {
+		fmt.Printf("error while copying directory: %v", err)
+	}
+	parser.DockerComposeParser("./tests/docker-compose.yaml")
+	err = ProcessBuildImage()
+	if err != nil {
+		fmt.Printf("error while processing build image: %v", err)
+	}
+
+}
