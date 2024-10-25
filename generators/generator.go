74102a873b9dd2e21e777877f479551ac6326108
kemboi22 <kemboielvis22@gmail.com>
2024-10-25 16:30:35 +0300

    feat(generator): few changes. Added ns8 volume generator

--- generators/generator.go
+++ generators/generator.go
@@ -1,1 +1,14 @@
 package generators
+
+import (
+	"fmt"
+	"strings"
+)
+
+func GenerateNS8VolumeFlags(volumes []string) string {
+	formattedVolume := ""
+	for _, volume := range volumes {
+		formattedVolume += fmt.Sprintf(" --volume %s", volume)
+	}
+	return strings.TrimSpace(formattedVolume)
+}
