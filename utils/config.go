d4631d0deb2bfd05dde182c982806e39aa55c8e5
kemboi22 <kemboielvis22@gmail.com>
2024-10-28 13:03:21 +0300

    feat: added config utils

---
+++ utils/config.go
@@ -0,0 +1,14 @@
+package utils
+
+var (
+	OutputDir   string
+	TemplateDir string
+)
+
+func SetOutputDir(dir string) {
+	OutputDir = dir
+}
+
+func SetTemplateDir(dir string) {
+	TemplateDir = dir
+}
