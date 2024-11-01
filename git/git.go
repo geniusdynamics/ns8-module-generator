ed28e6cffb7f5465ae0e5bb4c66dc7b6f45bce64
kemboi22 <kemboielvis22@gmail.com>
2024-11-02 02:40:30 +0300

    feat: added function to create repo

--- git/git.go
+++ git/git.go
@@ -1,7 +1,36 @@
 package git
 
-import "github.com/google/go-github/v66/github"
+import (
+	"context"
+	"fmt"
+	"ns8-module-generator/utils"
+
+	"github.com/google/go-github/v66/github"
+)
+
+var client *github.Client
 
 func InitilaizeGitClient() {
-	client := github.NewClient(nil)
+	// Get Github token
+	token := utils.GithubToken
+	// Create new git client
+	client = github.NewClient(nil).WithAuthToken(token)
+}
+
+func CreateRepository() error {
+	repo := &github.Repository{
+		Name:    github.String(""),
+		Private: github.Bool(false),
+	}
+	ctx := context.Background()
+	// Return response err and repo details
+	repo, _, err := client.Repositories.Create(ctx, utils.GithubOrganizationName, repo)
+	if err != nil {
+		return fmt.Errorf(
+			"An error occurred while occurred while creating the Repository: %v \n",
+			err,
+		)
+	}
+	fmt.Printf("The Git URL: %s", repo.GetGitURL())
+	return nil
 }
