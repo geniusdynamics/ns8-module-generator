fb0209830f4daa551a410e0e91bf829e62091a9b
kemboi22 <kemboielvis22@gmail.com>
2024-11-05 03:11:17 +0300

    feat: added git functions fot pushing and creating online repo

---
+++ git/git_folder.go
@@ -0,0 +1,80 @@
+package git
+
+import (
+	"fmt"
+	"ns8-module-generator/utils"
+
+	"github.com/go-git/go-git"
+	"gopkg.in/src-d/go-git.v4/config"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
+)
+
+func InitializeGit() error {
+	// Get Output path
+	outputPath := utils.OutputDir
+	// Initialize git repo
+	r, err := git.PlainInit(outputPath, false)
+	utils.SetGitLocalRepo(r)
+	// Check Err
+	if err != nil {
+		return fmt.Errorf("An Error occurred while initializing repository: %s", err)
+	}
+	// Get working tree
+	worktree, err := r.Worktree()
+	if err != nil {
+		return fmt.Errorf("An error occurred while getting work tree: %v", err)
+	}
+	// Set the working tree
+	utils.SetGitWorkingTree(worktree)
+
+	return nil
+}
+
+func GitAddFile(filePath string) error {
+	// Get current worktree
+	worktree := utils.GitWorkTree
+	// Add File
+	_, err := worktree.Add(filePath)
+	// Check for err
+	if err != nil {
+		return fmt.Errorf("An error occurred while adding file %s to git: %v", filePath, err)
+	}
+	return nil
+}
+
+func GitCommitFile(message string) error {
+	// Get Current work tree
+	worktree := utils.GitWorkTree
+	// Commit file
+	_, err := worktree.Commit(message, &git.CommitOptions{})
+	// Check for error
+	if err != nil {
+		return fmt.Errorf("An error occurred while commiting: %v", err)
+	}
+	return nil
+}
+
+func GitPushToRemote()error{
+  // Get current local repo
+  repo := utils.GitLocalRepo
+  // Set Remote Path 
+  _, err := repo.CreateRemote(&config.RemoteConfig{
+    Name: "origin",
+    URLs: [] string{ fmt.Sprintf(utils.GitRemoteUrl}
+  })
+  if err != nil {
+    return fmt.Errorf("An error occurred while adding remote config: %v", err)
+  }
+  err = repo.Push(&git.PushOptions{
+    RemoteName: "origin",
+    Auth: &http.BasicAuth{
+      Username: utils.GithubUsername,
+      Password: utils.GithubToken
+    }
+  })
+  if err != nil {
+    return fmt.Errorf("An error occurred while pushing online: %v", err)
+  }
+
+  return nil
+}
