1fb38f0b3c7c3d485c2182dc979b9ed6deb14e58
Kemboi Elvis <kemboielvis22@gmail.com>
2025-07-18 21:28:34 +0300

    feat: added backup and restore scripts for common databases

--- generators/backup.go
+++ generators/backup.go
@@ -7,29 +7,39 @@ import (
 )
 
 func AddToBackup(filePath, backupContent string) error {
-	// Check if file Exists
+	err := WriteToFile(filePath, backupContent, "feat(backup): added backup files needed")
+	return err
+}
+
+func WriteBackUpScript(filePath, backupContent string) error {
+	err := WriteToFile(filePath, backupContent, "feat: Added Backup COntent")
+	return err
+}
+
+func WriteToFile(filePath, fileContent, commitMessage string) error {
 	content, err := os.ReadFile(filePath)
 	// If error occurs Close
 	if err != nil {
-		return fmt.Errorf("Failed to read the file: %v", err)
+		return fmt.Errorf("failed to read the file: %v", err)
 	}
-	fmt.Println("Existing File Contents: ", string(content))
+	fmt.Println("existing file contents: ", string(content))
 
 	// Open file in append mode
 	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return fmt.Errorf("Failed to write to file: %v ", err)
+		return fmt.Errorf("failed to write to file: %v ", err)
 	}
 	// Close file later
 	defer file.Close()
-	if _, err := file.WriteString(backupContent + "\n"); err != nil {
-		return fmt.Errorf("Failed to add JSON DUMP in %s;", filePath)
+	if _, err := file.WriteString(fileContent + "\n"); err != nil {
+		return fmt.Errorf("failed to add json dump in %s;", filePath)
 	}
+	fmt.Print(fileContent)
 	err = git.GitAddFile(filePath)
 	if err != nil {
 		return err
 	}
-	err = git.GitCommitFiles("feat(backup): added backup files needed")
+	err = git.GitCommitFiles(commitMessage)
 	if err != nil {
 		return err
 	}
